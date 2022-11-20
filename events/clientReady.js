d182510d56418fccb4cae34b914300a8408f1ea1
Javier Flores <javierflores2209@outlook.com>
2022-11-20 04:46:47 -0300

    Initial commands & events

---
+++ events/clientReady.js
@@ -0,0 +1,5 @@
+module.exports = {
+    once: async () => {
+        console.log('Ready!')
+    }
+};
---
+++ events/interactionCreate.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+
+    on: async interaction => {
+        if (!interaction.isChatInputCommand()) return;
+    
+        const command = client.commands.get(interaction.commandName);
+    
+        if (!command) return;
+    
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }};
